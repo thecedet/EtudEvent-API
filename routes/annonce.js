dd169c804dc35e905dd7da59dcdbcd331f3f3046
thecedet <thecedet@outlook.fr>
2020-02-06 11:42:33 +0100

    add /annonce?page=0&?show=5

---
+++ routes/annonce.js
@@ -0,0 +1,39 @@
+const connection = require("../utils/mysql")
+
+const query = {
+    select: (page,show) => `
+SELECT
+annonce.uid, annonce.date, annonce.title, 
+user.firstName AS userFirstName, user.lastName AS userLastName
+FROM annonce 
+LEFT JOIN user 
+ON annonce.owner = user.uid
+ORDER BY annonce.date ASC
+LIMIT ${0 + page*show}, ${show + page*show}    
+`
+}
+
+
+module.exports = {
+    select: (request,response) => {
+
+        const page = parseInt(request.query.page) || 0
+        const show = parseInt(request.query.show) || 5
+
+        connection.query(query.select(page,show),(error, result ) => {
+            if(error) {
+                console.log(error)
+                response.status(500).send({
+                    result: "ERR_BDD"
+                })
+                return
+            }
+            if(result) {
+                response.status(200).send({
+                    result
+                })
+            }
+        })
+
+    }
+}
