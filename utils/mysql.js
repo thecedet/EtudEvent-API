9582cb140410b49259696c1b17c79983661aa366
thecedet <thecedet@outlook.fr>
2020-02-24 18:49:10 +0100

    bug fix mysql lost connection (i hope)

--- utils/mysql.js
+++ utils/mysql.js
@@ -7,26 +7,6 @@ const config =  {
   database : process.env.BDD_DATABASE,
 }
 
-let connection
-
-function handleDisconnect() {
-  connection = mysql.createConnection(config)
-  connection.connect(err => {              
-    if(err) {                                     
-      console.log('error when connecting to db:', err)
-      setTimeout(handleDisconnect, 2000); 
-    }                                     
-  })                                 
-
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-      handleDisconnect();                        
-    } else throw err;                                 
-
-  })
-}
-
-handleDisconnect();
+const connection = mysql.createPool(config)
 
 module.exports = connection
