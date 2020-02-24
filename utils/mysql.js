cf6ce61315a8596f23803d007265021bb1ede1b3
thecedet <thecedet@outlook.fr>
2020-02-24 12:35:31 +0100

    bug fix mysql protocol lost

--- utils/mysql.js
+++ utils/mysql.js
@@ -1,11 +1,32 @@
 const  mysql = require('mysql');
 
-connection = mysql.createConnection({
-  host     : process.env.BDD_HOST || 'sql2.freemysqlhosting.net',
-  user     : process.env.BDD_USER || 'sql2317009',
-  password : process.env.BDD_PASSWORD || 'mU8*rZ1*',
-  database : process.env.BDD_DATABASE || 'sql2317009'
-});
+const config =  {
+  host     : process.env.BDD_HOST,
+  user     : process.env.BDD_USER,
+  password : process.env.BDD_PASSWORD,
+  database : process.env.BDD_DATABASE,
+}
 
+let connection
+
+function handleDisconnect() {
+  connection = mysql.createConnection(config)
+  connection.connect(err => {              
+    if(err) {                                     
+      console.log('error when connecting to db:', err)
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  })                                 
+
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                        
+    } else throw err;                                 
+
+  })
+}
+
+handleDisconnect();
 
 module.exports = connection
