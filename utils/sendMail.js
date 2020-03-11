64148d83863a52261663e410a3c04281fe540076
thecedet <thecedet@outlook.fr>
2020-03-11 15:18:59 +0100

    test mail

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -9,7 +9,7 @@ module.exports = (to,subject,text) => {
         subject
     
     })({
-        html: mail
+        html: mail(text)
     }, (error, result, fullResult) => {
         if (error) console.error(error);
         console.log(result);
@@ -18,7 +18,7 @@ module.exports = (to,subject,text) => {
 }
 
 
-const mail = `
+const mail = text => `
 	<div style="background-color: #8B008B; padding: 10px; width: 80%; border-radius: 10px; margin-right: auto; margin-left: auto; padding: 2em;">
 	<table style="font-family: Arial; background-color: #FFFFFF; text-align: center; margin-right: auto; margin-left: auto; padding-top: 2em; border-radius: 10px">
 		<tr>
@@ -26,7 +26,7 @@ const mail = `
 			</td>
 		</tr>
 		<tr>
-			<td style="font-size: 1.3em;"> Pour v&eacute;rifier votre compte, veuillez cliquer sur le lien de v&eacute;rification suivant&nbsp;: <a href="inserer le lien plz">lien de v&eacute;rification</a>
+			<td style="font-size: 1.3em;"> Pour v&eacute;rifier votre compte, veuillez cliquer sur le lien de v&eacute;rification suivant&nbsp;: <a href="${text}">lien de v&eacute;rification</a>
 			</td>
 		</tr>
 		<tr>	
